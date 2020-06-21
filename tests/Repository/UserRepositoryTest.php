8aeeeaddb8da08bc35b09eb2fc28ffbdd5c2f0d0
Lounis Makhloufi <makhloufi.lounis@gmail.com>
2020-06-21 20:33:07 +0200

    update UserRepositoryTest

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -12,16 +12,23 @@ declare(strict_types=1);
 
 namespace App\Tests\Repository;
 
+use App\DataFixtures\UserFixtures;
 use App\Repository\UserRepository;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserRepositoryTest extends KernelTestCase
 {
 
+    use FixturesTrait;
+
     public function testCount()
     {
         self::bootKernel();
+        $this->loadFixtureFiles([
+            __DIR__ .'/UserRepositoryTestFixtures.yml'
+        ]);
         $users = self::$container->get(UserRepository::class)->count([]);
         $this->assertEquals(10, $users);
     }
