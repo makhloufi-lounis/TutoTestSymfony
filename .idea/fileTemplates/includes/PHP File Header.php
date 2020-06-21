0ca716554a7f7ef8c4a4c39ef49d0df20a013ec3
Lounis Makhloufi <makhloufi.lounis@gmail.com>
2020-06-21 17:56:34 +0200

    installation and configuration of the project

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,9 @@
+/**
+ * TutoTestSymfony
+ *
+ * @version   1.0
+ * @author    Lounis Makhloufi <makhloufi.lounis@gmail.com>
+ * @see       https://github.com/makhloufi-lounis/TutoTestSymfony for the canonical source repository
+ * @copyright Copyright (c) 2020. 
+ */
+ 
